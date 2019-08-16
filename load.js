d5c669ca83e79c0380778e2a43d612801e518115
h5mcbox <33593244+h5mcbox@users.noreply.github.com>
2019-08-16 13:27:28 +0800

    Update load.js

--- load.js
+++ load.js
@@ -1,6 +1,1 @@
-document.write("<script language=javascript src='http://translate.google.cn/translate_a/element.js?cb=googleTranslateElementInit'></script>");
-function googleTranslateElementInit() {
-            new google.translate.TranslateElement({
-                layout: google.translate.TranslateElement.InlineLayout.HORIZONTAL
-            }, 'google_translate_element');
-        }
+
