02e414f5f080c1dd690556a68d130d276832d172
h5mcbox <33593244+h5mcbox@users.noreply.github.com>
2020-05-06 07:28:07 +0800

    Update randstr.js

--- randstr.js
+++ randstr.js
@@ -1,1 +1,1 @@
-randstr=e=>{var randstr="";if(typeof e!=="number"){return undefined};for(let i=0;i<e;i++){randstr+="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"[Math.floor(Math.random()*52)]};return randstr;}
+randstr=e=>{var randstr="";if(typeof e!=="number"){return undefined};for(let i=0;i<e;i++){randstr+="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"[Math.floor(Math.random()*62)]};return randstr;}
