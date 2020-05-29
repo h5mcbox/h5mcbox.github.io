bd08497d98a550550de141327f3b88a9a0e646b3
h5mcbox <33593244+h5mcbox@users.noreply.github.com>
2020-05-29 21:12:11 +0800

    Update LoadScript.js

--- LoadScript.js
+++ LoadScript.js
@@ -1,1 +1,1 @@
-LoadScript=function(e){var script=document.createElement("script");script.src=e;document.body.appendChild(script);script.onload=function(){console.log("Script loaded.Script address is",e)}}
+LoadScript=function(e){var script=document.createElement("script");script.src=e;document.head.appendChild(script);script.onload=function(){console.log("Script loaded.Script address is",e)}}
