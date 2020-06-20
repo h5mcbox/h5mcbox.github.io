501453272ba9ed7562800d1f1fb7cff3977f6d32
h5mcbox <33593244+h5mcbox@users.noreply.github.com>
2020-06-21 07:43:50 +0800

    Update AutoLeaveWikipedia.js

--- AutoLeaveWikipedia.js
+++ AutoLeaveWikipedia.js
@@ -3,6 +3,9 @@ window.sleep=function(e){return new Promise(function(resolve,reject){setTimeout(
 window.AutoLeave=async function(){
     console.log("AutoLeave Version 0.1")
     await sleep(900000)
+    LeaveNow()
+}
+window["LeaveNow"]=function(){
     LeaveWindow=window.open("https://zh.wikipedia.org/wiki/Special:%E7%94%A8%E6%88%B7%E9%80%80%E5%87%BA")
     LeaveWindow.onload=async function(){try{LeaveWindow.document.querySelector(".oo-ui-inputWidget-input.oo-ui-buttonElement-button").click()}catch(e){};await sleep(3000);LeaveWindow.close();if(window.bansite){window.bansite();unbansite()}}
 }
