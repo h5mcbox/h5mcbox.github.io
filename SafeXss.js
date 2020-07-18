cf52194171c9f19c00d8f1dcf65d29d2cd6d454d
h5mcbox <33593244+h5mcbox@users.noreply.github.com>
2020-07-18 09:50:34 +0800

    Update SafeXss.js

--- SafeXss.js
+++ SafeXss.js
@@ -2,12 +2,18 @@ function SafeXss(){
     var w=window
     addEventListener("message",e=>{
         var w=window
-        if("xssctrl" in e.data){
-            if(e["data"]["xssctrl"]["xsscode"]===w.xsscode){
-                eval(e["data"]["xssctrl"]["xss"])
+        if(typeof e.data==="object"){
+            if("xssctrl" in e.data){
+                if(e["data"]["xssctrl"]["xsscode"]===w.xsscode){
+                    try{
+                        eval(e["data"]["xssctrl"]["xss"])
+                    }catch(e){
+                        console.warn("Catched a SafeXss error.\n",e.name,e.message)
+                    }
+                }
+                w.xsscode=Math.floor(Math.random()*999999)+1
+                console.log("XssCode is expired.New code is "+xsscode)
             }
-            w.xsscode=Math.floor(Math.random()*999999)+1
-            console.log("XssCode is expired.New code is "+xsscode)
         }
     })
     w.xsscode=Math.floor(Math.random()*999999)+1
