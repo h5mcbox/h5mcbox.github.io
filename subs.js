fbc9754a89b77f215f597e23f13de8433c3ea7e4
h5mcbox <33593244+h5mcbox@users.noreply.github.com>
2020-08-04 20:07:12 +0800

    Create subs.js

---
+++ subs.js
@@ -0,0 +1,26 @@
+function subs(){
+    var env={};
+    env.list=[];
+    env.add=function(f){
+        if(typeof f==="function"){
+            env.list.push(f);
+            return undefined;
+        }
+        throw "Not a function.";
+    }
+    env.toggle=function(...e){
+        env.list.forEach(o=>{
+            o.apply(window,e);
+        })
+    }
+    env.del=function(func){
+        if(env.list.includes(func)){
+            return delete env.list[env.list.indexOf(func)]
+        }
+        return false;
+    }
+    env.clear=function(){
+        env.list=[]
+    }
+    return {add:env.add,push:env.toggle,del:env.del,clear:env.clear};
+}
