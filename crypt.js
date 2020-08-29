82354632064041367b473b2174b112fd8905d1df
h5mcbox <33593244+h5mcbox@users.noreply.github.com>
2020-08-29 15:15:51 +0800

    Update crypt.js

--- crypt.js
+++ crypt.js
@@ -10,12 +10,12 @@ crypt={
             var t=e.charCodeAt(0)
             r.push(((t+p)^(b+p)).toString(c))
         })
-        return r.join("_")
+        return r.join(".")
     },
     decrypt:function(a,b,c){
         var p=2
         var c=c<=36?c:c%36;
-        var o=a.split("_")
+        var o=a.split(".")
         var r=[]
         o.forEach(e=>{
             p=p*7-4
