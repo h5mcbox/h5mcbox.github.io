65910a0f53076ed7ed9ffc9dba881724eb2bac3a
h5mcbox <33593244+h5mcbox@users.noreply.github.com>
2020-08-29 15:01:11 +0800

    Update crypt.js

--- crypt.js
+++ crypt.js
@@ -1,21 +1,27 @@
 crypt={
     encrypt:function(a,b,c){
+        var p=2
         var c=c<=36?c:c%36;
         var o=a.split("")
         var r=[]
         o.forEach(e=>{
+            p=p*7
+            p=p<64?p:p%64
             var t=e.charCodeAt(0)
-            r.push((t^b).toString(c))
+            r.push((t^(b+p)).toString(c))
         })
-        return r.join(String.fromCharCode(c+97))
+        return r.join("_")
     },
     decrypt:function(a,b,c){
+        var p=2
         var c=c<=36?c:c%36;
-        var o=a.split(String.fromCharCode(c+97))
+        var o=a.split("_")
         var r=[]
         o.forEach(e=>{
+            p=p*7
+            p=p<64?p:p%64
             var t=parseInt(e,c)
-            r.push(String.fromCharCode(t^b))
+            r.push(String.fromCharCode(t^(b+p)))
         })
         return r.join("")
     }
