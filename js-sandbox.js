2549e1fc86acc0a49786c5b3820b8904849fba13
h5mcbox <33593244+h5mcbox@users.noreply.github.com>
2020-08-31 17:40:01 +0800

    Update js-sandbox.js

--- js-sandbox.js
+++ js-sandbox.js
@@ -1,22 +1,31 @@
 function sandbox(code,sandboxParent=window){
-    var context=Object.create(sandboxParent)
-    var scontext={}
+    var context=Object.create(sandboxParent);
+    var scontext={};
     var FakedWindow=new Proxy(context,{
         get(obj,prop){
-            var t=prop
+            var t=prop;
+            if(typeof t!=="string"){
+                return undefined;
+            }
             if(t==="self"||t==="window"||t==="this"||t==="top"||t==="parent"||t==="globalThis"||t==="frames"){
-                return FakedWindow
+                return FakedWindow;
+            }
+            if(t.indexOf("_hidden_")==0){
+                return undefined;
             }
             if(!scontext[prop]&&typeof context[prop]==="object"){
-                scontext[prop]=Object.create(context[prop])
+                scontext[prop]=Object.create(context[prop]);
             }
-            return (typeof context[prop]==="object")?scontext[prop]:context[prop]
+            return (typeof context[prop]==="object")?scontext[prop]:context[prop];
         },
         has(){
-            return true
+            return true;
+        },
+        ownKeys(obj){
+            return Reflect.ownKeys(obj).filter(e=>e.indexOf("_hidden_")==0)
         }
     })
-    FakedWindow.runner=Function("proxy",`with(proxy){;${code};}`)
-    FakedWindow.runner(FakedWindow)
+    FakedWindow._hidden_=Function("proxy",`with(proxy){;${code};}`);
+    context._hidden_(FakedWindow);
     return [FakedWindow,context,scontext];
 }
