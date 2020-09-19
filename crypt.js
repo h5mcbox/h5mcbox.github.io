d48c02c261536c83f3058c1b4f2aa36a28859a86
h5mcbox <33593244+h5mcbox@users.noreply.github.com>
2020-09-19 11:53:07 +0800

    Update crypt.js

--- crypt.js
+++ crypt.js
@@ -1,6 +1,7 @@
 crypt={
-    encrypt:function(a,b,c){
-        var p=2
+    default_key:55,
+    encrypt:function(a,b=default_key,c=default_key,d=default_key){
+        var p=d
         var c=c<=36?c:c%36;
         var c=(c==1)?c+1:c
         var o=a.split("")
@@ -13,11 +14,11 @@ crypt={
         })
         return r.join(".")
     },
-    decrypt:function(a,b,c){
-        var p=2
+    decrypt:function(a,b=default_key,c=default_key,d=default_key){
+        var p=d
         var c=c<=36?c:c%36;
         var c=(c==1)?c+1:c
-        var o=a.split("_")
+        var o=a.split(".")
         var r=[]
         o.forEach(e=>{
             p=p*7-4
@@ -26,5 +27,11 @@ crypt={
             r.push(String.fromCharCode(t^(b+p)))
         })
         return r.join("")
+    },
+    encryptObj:function(obj,b=default_key,c=default_key,d=default_key){
+        return crypt.encrypt(JSON.stringify(obj),b,c,d);
+    },
+    decryptObj:function(text,b=default_key,c=default_key,d=default_key){
+        return JSON.parse(crypt.decrypt(text,b,c,d));
     }
 }
