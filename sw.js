8e878af3239d1f0e651a87e1e9d7d41c87484e45
h5mcbox <33593244+h5mcbox@users.noreply.github.com>
2020-10-03 07:54:13 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,17 +1,30 @@
 /**/
-var v="v4";
+var v="v5";
 var core=(async e=>{
     var cache=await caches.open(v);
     var value=await cache.match(e);
     if(value===undefined){
-        var f=await fetch(e)
+        try{
+            var f=await fetch(e)
+        }catch(f){
+            var f=await cache.match("error.html");
+        }
         console.log(f,f.clone())
         cache.put(e,f.clone());
         return f.clone();
     }
     return value;
 })
+var fileslist=[
+    "error.html",
+    "style.css"
+    ];
+var install=(async ()=>{
+    var cache=await caches.open(v);
+    await cache.addAll(fileslist);
+})
 addEventListener("install",()=>{
+    event.waitUntil(install())
     console.log("install");
 })
 addEventListener('fetch',function(e){
