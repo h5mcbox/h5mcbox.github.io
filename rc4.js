d33aa4ecfc6dea07395e82e0d2924f153a4aca3b
h5mcbox <33593244+h5mcbox@users.noreply.github.com>
2020-11-22 21:12:15 +0800

    Update rc4.js

--- rc4.js
+++ rc4.js
@@ -1,9 +1,7 @@
-//You need make a StringTools
 //Must be array
-rc4=function(data,key){
-    var datastream=strtools.encode(data), //encode data to binary
-        keystream=strtools.encode(key), //encode data to binary
-        ra=0,rb=0,rc=0,rd=0,re=0,rf=0,rg=0, //registers
+rc4=function(datastream,keystream){
+    var ra=0,rb=0,rc=0,rd=0,re=0,rf=0,rg=0, //registers
+        length=keystream.length,
         Sbox=[],
         result=[];
     var swap=function(a,b,c){
@@ -12,14 +10,14 @@ rc4=function(data,key){
         a[c]=d;
     }
     for(ra=0;ra<256;ra++){
-        keystream[ra]=keystream[ra%key.length];
+        keystream[ra]=keystream[ra%length];
         Sbox[ra]=ra;
     } //Fill keystream to 256 bits and make a S-box
     for(rb=0;rb<256;rb++){
         rc=(rc+Sbox[rb]+keystream[rb])%256;
         swap(Sbox,rb,rc)
     } //Everything is changing
-    for(rd=0;rd<data.length;rd++){
+    for(rd=0;rd<datastream.length;rd++){
         re=rd%256;
         rf=(rf+Sbox[re])%256;
         swap(Sbox,re,rf);
