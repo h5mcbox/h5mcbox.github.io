b2b272eff164c9ec3f3d4957aab6f940e3b2d21c
h5mcbox <33593244+h5mcbox@users.noreply.github.com>
2022-07-03 13:09:32 +0800

    More trips

--- sandbox1.js
+++ sandbox1.js
@@ -10,31 +10,75 @@
     var GeneratorFunction = (function* () { }).constructor;
     var context = Object.create(containerParent), errors = [];
     var DetectedObjects = [], ObjectContext = new WeakMap(), TrapedObjects = new WeakMap(), UntracedObjects = new WeakSet();
+    var customOwnKeys = function ownKeys(o) {
+      var result = Reflect.ownKeys(o);
+      result = result.filter(e => !(Blacklist.has(e)));
+      if (ObjectContext.has(o)) {
+        var resultSet = new Set(result);
+        Object.keys(ObjectContext.get(o)).forEach(e => {
+          resultSet.add(e);
+        });
+        result = [...resultSet];
+      }
+      return result;
+    }
     var TrapObject = function TrapObject(object) {
       if (TrapedObjects.has(object)) return TrapedObjects.get(object)
       if (!(typeof object === "object" || typeof object === "function")) throw "The target must be an object or an function.";
       var result = new Proxy(object, {
         get(obj, key) {
-          if (redirects.has(obj[key])) {
-            return redirects.get(obj[key]);
-          } else if (Blacklist.has(obj[key])) {
-            throw `The access to ${key} has blocked.`;
-          } else if ((typeof obj[key] === "object" || typeof obj[key] === "function") && (!UntracedObjects.has(obj[key]))) {
-            return TrapObject(obj[key]);
-          }
+          var r1;
           if (ObjectContext.has(obj)) {
-            return ObjectContext.get(obj)[key] || obj[key];
+            r1 = Reflect.get(ObjectContext.get(obj), key) || obj[key];
           } else {
-            return obj[key];
+            r1 = Reflect.get(obj, key);
           }
+          if (redirects.has(r1)) {
+            return redirects.get(r1);
+          } else if (Blacklist.has(r1)) {
+            throw `The access to ${key} has blocked.`;
+          } else if ((typeof r1 === "object" || typeof r1 === "function") && (!UntracedObjects.has(r1))) {
+            return TrapObject(r1);
+          }
+          return r1;
         },
         set(obj, key, value) {
           if (!ObjectContext.has(obj)) {
             ObjectContext.set(obj, {});
           }
-          ObjectContext.get(obj)[key] = value;
-          return true;
-        }
+          if (redirects.has(value)) {
+            return Reflect.set(obj, key, redirects.get(value));
+          }
+          return Reflect.set(ObjectContext.get(obj), key, value);
+        },
+        defineProperty(o, p, a) {
+          if (!ObjectContext.has(obj)) {
+            ObjectContext.set(obj, {});
+          }
+          return Reflect.defineProperty(ObjectContext.get(o), p, a);
+        },
+        getOwnPropertyDescriptor(o, p) {
+          var r1;
+          if (ObjectContext.has(o)) {
+            r1 = Reflect.getOwnPropertyDescriptor(ObjectContext.get(o), p) || Reflect.getOwnPropertyDescriptor(o, p);
+          } else {
+            r1 = Reflect.getOwnPropertyDescriptor(o, p);
+          }
+          if(!r1){return r1;}
+          if ("value" in r1) {
+            if (redirects.has(r1.value)) {
+              r1.value = redirects.get(r1.value);
+              return r1;
+            } else if (Blacklist.has(r1.value)) {
+              throw `The access to ${p} has blocked.`;
+            } else if ((typeof r1.value === "object" || typeof r1.value === "function") && (!UntracedObjects.has(r1.value))) {
+              r1.value = TrapObject(Reflect.get(o, p));
+              return r1;
+            }
+          }
+          return r1;
+        },
+        ownKeys: customOwnKeys
       });
       TrapedObjects.set(object, result);
       return result;
@@ -43,7 +87,10 @@
       get(obj, key) {
         if (key === "undefined") { return undefined; }
         if (key === "null") { return null; }
-        if (typeof key !== "string") {
+        if ((typeof key !== "string") && (typeof key !== "symbol")) {
+          return undefined;
+        }
+        if (key === Symbol.unscopables) {
           return undefined;
         }
         if (readOnce.has(key)) {
@@ -52,25 +99,29 @@
           return result;
         }
         if (!(key in obj)) {
-          throw new ReferenceError(`${key} is not defined`);
+          var errmsg;
+          if (typeof key === "symbol") {
+            errmsg = `Symbol(${key.description}) is not defined`;
+          } else {
+            errmsg = `${key} is not defined`;
+          }
+          throw new ReferenceError(errmsg);
         }
-        var result = obj[key];
+        var result = Reflect.get(obj, key);
         if (Blacklist.has(result)) {
           throw `The access to ${key} has blocked.`;
         }
-        if ((typeof result === "object" || typeof result === "function") && (!UntracedObjects.has(obj[key]))) {
-          return TrapObject(obj[key]);
+        if ((typeof result === "object" || typeof result === "function") && (!UntracedObjects.has(result))) {
+          return TrapObject(result);
         }
-        return obj[key];
+        return result;
       },
       set: function _set(obj, key, value) {
         if (TrapedObjects.has(value)) {
-          obj[key] = value;
-          return true;
+          return Reflect.set(obj, key, TrapedObjects.get(value));
         }
         if (redirects.has(value)) {
-          obj[key] = redirects.get(value);
-          return true;
+          return Reflect.set(obj, key, redirects.get(value));
         } else {
           if (typeof value === "object" || typeof value === "function") {
             Object.keys(value).forEach(function (e) {
@@ -81,13 +132,63 @@
               }
             });
           }
-          obj[key] = value;
-          return true;
+          return Reflect.set(obj, key, value);
         }
       },
       has() {
         return true;
-      }
+      },
+      defineProperty: function _defineProperty(o, p, a) {
+        if ("value" in a) {
+          if (TrapedObjects.has(value)) {
+            a.value = TrapedObjects.get(value)
+            return Reflect.defineProperty(o, p, a);
+          }
+          if (redirects.has(value)) {
+            a.value = redirects.get(value);
+            return Reflect.defineProperty(o, p, a);
+          } else {
+            if (typeof value === "object" || typeof value === "function") {
+              Object.keys(value).forEach(function (e) {
+                var _value = value[e];
+                if (!DetectedObjects.includes(_value)) {
+                  DetectedObjects.push(_value);
+                  return _defineProperty(value, e, _value);
+                }
+              });
+            }
+            Reflect.defineProperty(o, p, a);
+          }
+        } else {
+          Reflect.defineProperty(o, p, a);
+        }
+      },
+      getOwnPropertyDescriptor(o, p) {
+        var r1;
+        if (p === "undefined") { return { value: undefined, writable: false, enumerable: false, configurable: false } }
+        if (p === "null") { return { value: null, writable: false, enumerable: false, configurable: false } }
+        if ((typeof p !== "string") && (typeof p !== "symbol")) {
+          return undefined;
+        }
+        if (readOnce.has(p)) {
+          var result = readOnce.get(p);
+          readOnce.delete(p);
+          return { value: result, writable: false, enumerable: true, configurable: false };
+        }
+        r1 = Reflect.getOwnPropertyDescriptor(o, p);
+        if(!r1){return r1;}
+        if ("value" in r1) {
+          if (Blacklist.has(r1.value)) {
+            throw `The access to ${p} has blocked.`;
+          }
+          if ((typeof r1.value === "object" || typeof r1.value === "function") && (!UntracedObjects.has(r1.value))) {
+            r1.value = TrapObject(r1.value);
+            return r1;
+          }
+        }
+        return r1;
+      },
+      ownKeys: customOwnKeys
     });
     var isNODEJS = Boolean(typeof module !== "undefined");
     var _global = isNODEJS ? global : window;
@@ -180,26 +281,14 @@
       RestorePrototype(GeneratorFunction);
       return result;
     }
-    var ContainerFunction = function (code = "", noReturn = false) {
+    var ContainerFunction = function (code = "") {
       try {
         Function(code);
       } catch (error) {
         errors.push(error);
         throw error;
       }
-      var result;
-      try {
-        if (noReturn) throw "";
-        result = _InternalRun(`return ${code}`);
-      } catch (error) {
-        if (error instanceof SyntaxError) {
-          if (error.message.startsWith("Unexpected token")) {
-            throw error;
-          }
-        }
-        if (!(typeof error === "string" || noReturn)) throw error;
-        result = _InternalRun(code);
-      }
+      result = _InternalRun(code);
       return result;
     };
     return {
